4c65c179e4906f6dea6f2af8d34263f8cedd3c67
Marina Master <marina777master@mail.ru>
2023-07-31 20:19:49 -0400

    complite excuse generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
+import { excuses } from "../generator.js";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+
+  var excuse = excuses();
+  var h1 = document.querySelector("#excuse");
+  h1.textContent = excuse;
   console.log("Hello Rigo from the console!");
 };
